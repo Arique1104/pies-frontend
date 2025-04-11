6406fb785e6de2552d2051fe36d000d977c351c6
Arique <arique1104@gmail.com>
2025-04-10 18:14:21 -0700

    files for connecting PIES to backend

---
+++ src/api/axios.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: 'http://localhost:3000', // Rails backend
+    headers: {
+        'Content-Type': 'application/json',
+
+    },
+});
+
+instance.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token && config.headers){
+        config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+});
+
+export default instance;
