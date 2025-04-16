2ffb01b58065f4f8763394f034b13222fb066e76
Arique <arique1104@gmail.com>
2025-04-16 13:49:46 -0700

    updates routes for application

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,40 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
 import ProtectedRoute from './components/ProtectedRoute';
 import OwnerSignup from './pages/OwnerSignup';
 import LeaderSignup from './pages/LeaderSignup.tsx';
 import UnmatchedKeywordTipFormPage from './components/UnmatchedKeywordTipForm.tsx';
 import UnmatchedKeywords from './components/UnmatchedKeywords.tsx';
+import ReflectionTipsManager from './components/ReflectionTipsManager.tsx';
+import SuggestedMatches from './components/SuggestedMatches.tsx';
+import TeamManagement from './components/TeamManagement.tsx';
+import OwnerDashboard from './components/OwnerDashboard';
 
 function App() {
   return (
     <BrowserRouter>
-    <Navbar />
+      <Navbar />
       <Routes>
-        <Route path='/secret-owner-signup' element={<OwnerSignup/>}/>
-        <Route path='/secret-leader-signup' element={<LeaderSignup/>}/>
+        <Route path="/secret-owner-signup" element={<OwnerSignup />} />
+        <Route path="/secret-leader-signup" element={<LeaderSignup />} />
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/unmatched_keywords" element={<UnmatchedKeywords />} />
         <Route path="/unmatched_keywords/:id/new_tip" element={<UnmatchedKeywordTipFormPage />} />
 
+        {/* Owner Dashboard (Protected + Nested Views) */}
         <Route path="/dashboard" element={
           <ProtectedRoute>
-            <Dashboard />
+            <OwnerDashboard />
           </ProtectedRoute>
-          } />
-
+        }>
+          <Route path="suggested_matches" element={<SuggestedMatches />} />
+          <Route path="teams" element={<TeamManagement />} />
+          <Route path="unmatched_keywords" element={<UnmatchedKeywords />} />
+          <Route path="reflection_tips" element={<ReflectionTipsManager userRole="owner" />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
